5744d1da9ef6d7a74ef69186a6baf9dff2420a42
Joel Wakefield <joeltwakefield@gmail.com>
2022-08-27 01:15:03 -0600

    utilize barrel file for App

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import StormButton from '../components/StormButton';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <StormButton color='primary' />
+      </header>
+    </div>
+  );
+}
+
+export default App;
